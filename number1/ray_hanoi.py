8d9dc77ad3cd460c3f2e9ef44e59ddc607d5abf2
Raymond Sutrisno <raymond@sutrisno.me>
2018-01-29 18:11:37 -0600

    some trimmings

--- number1/ray_hanoi.py
+++ number1/ray_hanoi.py
@@ -5,18 +5,15 @@ class HanoiPillar:
         self._stack = [i+1 for i in reversed(range(high))] if high >= 1 else []
 
     def __mul__(self, b):
-        """ This is used to represent movement
+        """
+            This is used to represent movement
         """
         def check_order(a):
-            for ii in range(len(a) - 1):
-                if a[ii] < a[ii + 1]:
-                    raise InvalidMoveException
-                
-        if len(b._stack) > 0:
-            a_high = self._stack[0]
-            b_high = self._stack[0]
+            if any([a[i] < a[i+1] for i in range(len(a) - 1)]):
+                raise InvalidMoveException
+
         b._stack.append(self._stack.pop())
-        check_order(self._stack)
+        check_order(self._stack) # not sure why we need this
         check_order(b._stack)
 
         return b
@@ -24,8 +21,10 @@ class HanoiPillar:
     def __repr__(self):
         return str(self._stack)
 
+
 def hanoi(t1, t2, t3, t4, t5, n):
-    if n:
+    if n > 0:
+        # TO-DO print pillars in right order
         hanoi(t1, t2, t3, t4, t5, n-1)
         t1 * t2 * t3 * t4
         hanoi(t5, t4, t3, t2, t1, n-1)
@@ -34,16 +33,16 @@ def hanoi(t1, t2, t3, t4, t5, n):
 
 
 if __name__ == '__main__':
-    n = 100
-    t1 = HanoiPillar(n)
-    t2 = HanoiPillar()
-    t3 = HanoiPillar()
-    t4 = HanoiPillar()
-    t5 = HanoiPillar()
+    n = 5
+    P1 = HanoiPillar(n)
+    P2 = HanoiPillar()
+    P3 = HanoiPillar()
+    P4 = HanoiPillar()
+    P5 = HanoiPillar()
 
-    print(t1, t2, t3, t4, t5)
-    hanoi(t1, t2, t3, t4, t5, n)
-    print(t1, t2, t3, t4, t5)
+    print(P1, P2, P3, P4, P5)
+    hanoi(P1, P2, P3, P4, P5, n)
+    print(P1, P2, P3, P4, P5)
 
 
 
