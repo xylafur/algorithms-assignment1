2c04cc278a11eefcb070fbfe94e36235afb75d17
Raymond Sutrisno <raymond@sutrisno.me>
2018-01-29 21:06:50 -0600

    I FINALLY DID HANOI

--- number1/ray_hanoi.py
+++ number1/ray_hanoi.py
@@ -8,6 +8,11 @@ class HanoiPillar:
         """
             This is used to represent movement
         """
+        if self._stack == b._stack:
+            return self
+        if len(self._stack) == 0:
+            raise InvalidMoveException
+
         def check_order(a):
             if any([a[i] < a[i+1] for i in range(len(a) - 1)]):
                 raise InvalidMoveException
@@ -22,29 +27,51 @@ class HanoiPillar:
         return str(self._stack)
 
 
-def hanoi(t1, t2, t3, t4, t5, n):
+
+n = 10
+P1 = HanoiPillar(n)
+P2 = HanoiPillar()
+P3 = HanoiPillar()
+P4 = HanoiPillar()
+P5 = HanoiPillar()
+
+def hanoi(pillars, n):
     if n > 0:
-        # TO-DO print pillars in right order
-        hanoi(t1, t2, t3, t4, t5, n-1)
-        t1 * t2 * t3 * t4
-        hanoi(t5, t4, t3, t2, t1, n-1)
-        t4 * t5
-        hanoi(t1, t2, t3, t4, t5, n-1)
+        if len(pillars) == 3:
+            t1, t2, t3 = tuple(pillars)
+            hanoi([t1, t2, t3], n-1)
+
+            t1 * t2
+#            print(P1, P2, P3, P4, P5)
+
+            hanoi([t3, t2, t1], n-1)
+
+            t2 * t3
+#            print(P1, P2, P3, P4, P5)
+
+            hanoi([t1, t2, t3], n-1)
+
+        elif len(pillars) == 5:
+            t1,t2,t3,t4,t5 = tuple(pillars)
+
+            hanoi([t1, t2, t3, t4, t4], n-1)
+
+            t1 * t2 * t3
+#            print(P1, P2, P3, P4, P5)
+
+            hanoi([t4, t3, t2], n-1)
+
+            t3 * t4 * t5
+#            print(P1, P2, P3, P4, P5)
+
+            hanoi([t2, t2, t3, t4, t5], n-1)
 
 
-if __name__ == '__main__':
-    n = 5
-    P1 = HanoiPillar(n)
-    P2 = HanoiPillar()
-    P3 = HanoiPillar()
-    P4 = HanoiPillar()
-    P5 = HanoiPillar()
-
-    print(P1, P2, P3, P4, P5)
-    hanoi(P1, P2, P3, P4, P5, n)
-    print(P1, P2, P3, P4, P5)
 
 
-
+print('Initial state:\n', P1, P2, P3, P4, P5)
+print('Running hanoi\n')
+hanoi([P1, P2, P3, P4, P5], n)
+print(P1, P2, P3, P4, P5)
 
 
