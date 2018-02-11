2fef0092bc86949178b6ee46bd1e401a5cd37839
xylafur <jkesleyrichardson@gmail.com>
2018-02-11 17:02:42 -0600

    Updated fragment

--- number4/fragment.c
+++ number4/fragment.c
@@ -20,7 +20,7 @@ struct timespec diff(struct timespec start, struct timespec end)
 
 /*  Function to fragment memory.
  */
-void fragment(int m, int d)
+void fragment(int m, int d, int e)
 {
     int ** ar1 = malloc(3 * m * sizeof(int *));
     int ** ar2 = malloc(m * sizeof(int *));
@@ -30,37 +30,57 @@ void fragment(int m, int d)
     int sum = 0;
 
     printf("Running fragment with m of value %d\n", m);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_start);
+    printf("First allocation\n");
+    if(!e)
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_start);
     for(i = 0; i < 3 * m; i++){
+        if(e && (i <= 10 || i >= (3 * m - 10)))
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_start);
         ar1[i] = malloc(800000 * sizeof(int));
         if(ar1[i] == 0){
             printf("Error, malloc failed\n");
             exit(1);
         }
+        if(e && (i <= 10 || i >= (3 * m - 10))){
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_end);
+            alloc1 = diff(alloc1_start, alloc1_end);
+            printf("Time1 i = %d: %d seconds, %lu nanoseconds\n", i, alloc1.tv_sec, alloc1.tv_nsec);
+        }
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_end);
-    alloc1 = diff(alloc1_start, alloc1_end);
-    printf("Time1: %d seconds, %lu nanoseconds\n", alloc1.tv_sec, alloc1.tv_nsec);
+    if(!e){
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc1_end);
+        alloc1 = diff(alloc1_start, alloc1_end);
+        printf("Time1: %d seconds, %lu nanoseconds\n", alloc1.tv_sec, alloc1.tv_nsec);
+    }
 
     for(i = 0; i < 3 * m; i+=2)
         free(ar1[i]);
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_start);
+    printf("Second allocation\n");
+    if(!e)
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_start);
     for(i = 0; i < m; i++){
+        if(e && (i <= 10 || i >= m - 10))
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_start);
         ar2[i] = malloc(900000 * sizeof(int));
         if(ar2[i] == 0){
             printf("Error, malloc failed\n");
             exit(1);
         }
-        
+        if(e && (i <= 10 || i >= m - 10)){
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_end);
+            alloc2 = diff(alloc2_start, alloc2_end);
+            printf("Time2 i = %d: %d seconds, %lu nanoseconds\n", i, alloc2.tv_sec, alloc2.tv_nsec);
+        }
     }
 
     printf("\n");
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_end);
-
-    alloc2 = diff(alloc2_start, alloc2_end);
-    printf("Time1: %d seconds, %lu nanoseconds\n", alloc2.tv_sec, alloc2.tv_nsec);
+    if(!e){
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &alloc2_end);
+        alloc2 = diff(alloc2_start, alloc2_end);
+        printf("Time1: %d seconds, %lu nanoseconds\n", alloc2.tv_sec, alloc2.tv_nsec);
+    }
 
     if(d){
         printf("Addresses for array 1\n");
@@ -93,15 +113,18 @@ int main(int argc, char * argv [])
         printf("Need to supply default value for m as a parameter\n");
         exit(1);
     }
-    int m, d = 0;
+    int m, d = 0, e = 0;
     char y [256];
 
-    if(argc == 3)
+    if(argc >= 3)
         d = 1;
+    if(argc == 4)
+        e = 1;
+    
 
     m = atoi(argv[1]);
     do{
-        fragment(m, d);
+        fragment(m, d, e);
         m++;
         break;
     }while(0);
